76bccd71a9b5438bf7beee48aa74a86bc50232dd
ceylanomer <omer-ceylan@hotmail.com>
2025-02-24 02:27:55 +0300

    add request handling utility and OpenTelemetry tracer initialization

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"context"
-	"errors"
 	"fmt"
 	"github.com/gofiber/contrib/otelfiber/v2"
 	"github.com/gofiber/fiber/v2"
@@ -10,104 +8,20 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/basicauth"
 	recover "github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
-	"go.opentelemetry.io/otel/propagation"
-	"go.opentelemetry.io/otel/sdk/resource"
-	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 	"go.uber.org/zap"
 	"golang-fiber-poc/app/client"
 	"golang-fiber-poc/app/healthcheck"
 	"golang-fiber-poc/app/product"
 	"golang-fiber-poc/infra/couchbase"
 	"golang-fiber-poc/pkg/config"
+	"golang-fiber-poc/pkg/handler"
 	_ "golang-fiber-poc/pkg/log"
+	"golang-fiber-poc/pkg/tracer"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 )
-
-type Request any
-type Response any
-
-type HandlerInterface[R Request, Res Response] interface {
-	Handle(ctx context.Context, req *R) (*Res, error)
-}
-
-func handle[R Request, Res Response](handler HandlerInterface[R, Res]) fiber.Handler {
-	return func(c *fiber.Ctx) error {
-		var req R
-
-		if err := c.BodyParser(&req); err != nil && !errors.Is(err, fiber.ErrUnprocessableEntity) {
-			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
-		}
-
-		if err := c.ParamsParser(&req); err != nil {
-			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
-		}
-
-		if err := c.QueryParser(&req); err != nil {
-			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
-		}
-
-		if err := c.ReqHeaderParser(&req); err != nil {
-			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
-		}
-
-		/*
-			ctx, cancel := context.WithTimeout(c.UserContext(), 3*time.Second)
-			defer cancel()
-		*/
-
-		ctx := c.UserContext()
-
-		res, err := handler.Handle(ctx, &req)
-		if err != nil {
-			zap.L().Error("Failed to handle request", zap.Error(err))
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
-		}
-
-		return c.JSON(res)
-	}
-}
-
-//For V3
-//func handle[R Request, Res Response](handler HandlerInterface[R, Res]) fiber.Handler {
-//	return func(c fiber.Ctx) error {
-//		var req R
-//
-//		if err := c.Bind().Body(&req); err != nil && !errors.Is(err, fiber.ErrUnprocessableEntity) {
-//			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
-//		}
-//
-//		if err := c.Bind().Query(&req); err != nil {
-//			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
-//		}
-//
-//		if err := c.Bind().Header(&req); err != nil {
-//			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
-//		}
-//
-//		if err := c.Bind().URI(&req); err != nil {
-//			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
-//		}
-//
-//		ctx, cancel := context.WithTimeout(c.Context(), 3*time.Second)
-//		defer cancel()
-//
-//		res, err := handler.Handle(ctx, &req)
-//
-//		if err != nil {
-//			zap.L().Error("Failed to handle request", zap.Error(err))
-//			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
-//		}
-//
-//		return c.JSON(res)
-//	}
-//}
 
 func main() {
 	appConfig := config.Read()
@@ -117,7 +31,7 @@ func main() {
 
 	httpClient := client.NewHttpClient()
 
-	tp := initTracer()
+	tp := tracer.InitTracer()
 	couchbaseRepository := couchbase.NewRepository(tp)
 
 	healthcheckHandler := healthcheck.NewHealthCheckHandler()
@@ -135,7 +49,7 @@ func main() {
 	app.Use(recover.New())
 	app.Use(otelfiber.Middleware())
 
-	app.Get("/healthcheck", handle[healthcheck.Request, healthcheck.Response](healthcheckHandler))
+	app.Get("/healthcheck", handler.Handle[healthcheck.Request, healthcheck.Response](healthcheckHandler))
 	app.Get("/metrics", adaptor.HTTPHandler(promhttp.Handler()))
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
@@ -154,9 +68,9 @@ func main() {
 		},
 	}))
 
-	productGroup.Get("/:id", handle[product.GetProductRequest, product.GetProductResponse](getProductHandler))
-	productGroup.Post("/", handle[product.CreateProductRequest, product.CreateProductResponse](createProductHandler))
-	productGroup.Put("/:id", handle[product.UpdateProductRequest, product.UpdateProductResponse](updateProductHandler))
+	productGroup.Get("/:id", handler.Handle[product.GetProductRequest, product.GetProductResponse](getProductHandler))
+	productGroup.Post("/", handler.Handle[product.CreateProductRequest, product.CreateProductResponse](createProductHandler))
+	productGroup.Put("/:id", handler.Handle[product.UpdateProductRequest, product.UpdateProductResponse](updateProductHandler))
 
 	go func() {
 		if err := app.Listen(fmt.Sprintf(":%s", appConfig.Port)); err != nil {
@@ -168,39 +82,6 @@ func main() {
 	zap.L().Info("Server started on port", zap.String("port", appConfig.Port))
 
 	gracefulShutdown(app)
-}
-
-func initTracer() *sdktrace.TracerProvider {
-
-	headers := map[string]string{
-		"content-type": "application/json",
-	}
-
-	exporter, err := otlptrace.New(
-		context.Background(),
-		otlptracehttp.NewClient(
-			otlptracehttp.WithEndpoint("localhost:4318"),
-			otlptracehttp.WithHeaders(headers),
-			otlptracehttp.WithInsecure(),
-		),
-	)
-	if err != nil {
-		zap.L().Fatal("Failed to create stdout exporter", zap.Error(err))
-	}
-
-	tp := sdktrace.NewTracerProvider(
-		sdktrace.WithSampler(sdktrace.AlwaysSample()),
-		sdktrace.WithBatcher(exporter),
-		sdktrace.WithResource(
-			resource.NewWithAttributes(
-				semconv.SchemaURL,
-				semconv.ServiceNameKey.String("golang-fiber-poc"),
-			)),
-	)
-
-	otel.SetTracerProvider(tp)
-	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
-	return tp
 }
 
 func gracefulShutdown(app *fiber.App) {
---
+++ pkg/tracer/tracer.go
@@ -0,0 +1,46 @@
+package tracer
+
+import (
+	"context"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/semconv/v1.4.0"
+	"go.uber.org/zap"
+)
+
+func InitTracer() *trace.TracerProvider {
+
+	headers := map[string]string{
+		"content-type": "application/json",
+	}
+
+	exporter, err := otlptrace.New(
+		context.Background(),
+		otlptracehttp.NewClient(
+			otlptracehttp.WithEndpoint("localhost:4318"),
+			otlptracehttp.WithHeaders(headers),
+			otlptracehttp.WithInsecure(),
+		),
+	)
+	if err != nil {
+		zap.L().Fatal("Failed to create stdout exporter", zap.Error(err))
+	}
+
+	tp := trace.NewTracerProvider(
+		trace.WithSampler(trace.AlwaysSample()),
+		trace.WithBatcher(exporter),
+		trace.WithResource(
+			resource.NewWithAttributes(
+				semconv.SchemaURL,
+				semconv.ServiceNameKey.String("golang-fiber-poc"),
+			)),
+	)
+
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+	return tp
+}
